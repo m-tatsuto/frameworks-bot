647d56d23ca150075481d82b58cc0c0d68a54e87
t.maetsu <t.maetsu@lexues.co.jp>
2015-03-20 11:04:06 +0900

    文言修正

--- bin/index.js
+++ bin/index.js
@@ -5,27 +5,27 @@ var moment = require('moment');
 //var http = require('http');
 
 var frameworkTweets = [
- "Phalcon \n\nThe fastest PHP Framework. #PHP #Framework\n\nhttp://phalconphp.com/ \n\n",
- "AngularJS \n\nHTML enhanced for web apps! #Javascript #Framework\n\nhttps://angularjs.org/",
- "Laravel \n\nThe PHP Framework For Web Artisans #PHP #Framework\n\nhttp://laravel.com/",
- "Play \n\nThe High Velocity Web Framework For Java and Scala #Java #Scala #Framework \n\nhttps://www.playframework.com/",
- "Ruby on Rails \n\nWeb development that doesn’t hurt #Ruby #Framework\n\nhttp://rubyonrails.org/",
- "CakePHP \n\n #PHP #Framework\n\nhttp://cakephp.org/",
- "Underscore.js \n\n #Javascript #Framework\n\nhttp://underscorejs.org/",
- "BootStrap \n\n HTML,CSS,JS framework for developing responsive, mobile first projects on the web.#HTML #CSS\n\nhttp://getbootstrap.com/",
- "Sinatra \n\nDSL for quickly creating web applications #Ruby #Framework \n\nhttp://www.sinatrarb.com/",
- "Backbone.js \n\n #Javascript #Framework\n\nhttp://backbonejs.org/",
- "jQuery \n\n Fast, small, and feature-rich JavaScript library #Javascript\n\nhttp://jquery.com/",
- "Materialize \n\nA modern responsive front-end framework based on Material Design #CSS #Framework\n\nhttp://materializecss.com/",
- "MEAN.JS \n\nOpen-Source Full-Stack Solution For MEAN Applications #Javascript #Framework\n\nhttp://meanjs.org/",
- "Django \n\nDjango makes it easier to build better Web apps more quickly and with less code. #Python #Framework\n\nhttps://www.djangoproject.com/",
- "Drupal \n\nCome for the software, stay for the community #CMS #Framework\n\nhttps://www.drupal.org/",
- "CodeIgniter \n\n #PHP #Framework\n\nhttp://www.codeigniter.com/",
- "CapeDwarf \n\nCapeDwarf is an open-source alternative to Google App Engine #GoogleAppEngine\n\nhttp://capedwarf.org/",
- "Aurelia \n\nAurelia is a next generation JavaScript client framework #Javascript #Framework\n\nhttp://aurelia.io/",
- "Socket.io \n\nFEATURING THE FASTEST AND MOST RELIABLE REAL-TIME ENGINE #Javascript #NodeJS\n\nhttp://socket.io/",
- "OpenShift \n\nDEVELOP, HOST, AND SCALE YOUR APPS IN THE CLOUD #PaaS #Redhat\n\nhttps://www.openshift.com/",
- "Heroku \n\nFocus on the app #PaaS\n\nhttp://heroku.com/"
+ "Phalcon #PHP #Framework\n\nThe fastest PHP Framework.\n\nhttp://phalconphp.com/ \n\n",
+ "AngularJS #Javascript #Framework\n\nHTML enhanced for web apps!\n\nhttps://angularjs.org/",
+ "Laravel #PHP #Framework\n\nThe PHP Framework For Web Artisans\n\nhttp://laravel.com/",
+ "Play #Java #Scala #Framework\n\nThe High Velocity Web Framework For Java and Scala\n\nhttps://www.playframework.com/",
+ "Ruby on Rails #Ruby #Framework\n\nWeb development that doesn’t hurt\n\nhttp://rubyonrails.org/",
+ "CakePHP #PHP #Framework\n\n\n\nhttp://cakephp.org/",
+ "Underscore.js #Javascript #Framework\n\n\n\nhttp://underscorejs.org/",
+ "BootStrap #HTML #CSS\n\n HTML,CSS,JS framework for developing responsive, mobile first projects on the web.\n\nhttp://getbootstrap.com/",
+ "Sinatra #Ruby #Framework\n\nDSL for quickly creating web applications \n\nhttp://www.sinatrarb.com/",
+ "Backbone.js #Javascript #Framework\n\n\n\nhttp://backbonejs.org/",
+ "jQuery #Javascript\n\n Fast, small, and feature-rich JavaScript library\n\nhttp://jquery.com/",
+ "Materialize #CSS #Framework\n\nA modern responsive front-end framework based on Material Design\n\nhttp://materializecss.com/",
+ "MEAN.JS #Javascript #Framework\n\nOpen-Source Full-Stack Solution For MEAN Applications\n\nhttp://meanjs.org/",
+ "Django #Python #Framework\n\nDjango makes it easier to build better Web apps more quickly and with less code.\n\nhttps://www.djangoproject.com/",
+ "Drupal #CMS #Framework\n\nCome for the software, stay for the community\n\nhttps://www.drupal.org/",
+ "CodeIgniter #PHP #Framework\n\n\n\nhttp://www.codeigniter.com/",
+ "CapeDwarf #GoogleAppEngine\n\nCapeDwarf is an open-source alternative to Google App Engine\n\nhttp://capedwarf.org/",
+ "Aurelia #Javascript #Framework\n\nAurelia is a next generation JavaScript client framework\n\nhttp://aurelia.io/",
+ "Socket.io #Javascript #NodeJS\n\nFEATURING THE FASTEST AND MOST RELIABLE REAL-TIME ENGINE\n\nhttp://socket.io/",
+ "OpenShift #PaaS #Redhat\n\nDEVELOP, HOST, AND SCALE YOUR APPS IN THE CLOUD\n\nhttps://www.openshift.com/",
+ "Heroku #PaaS\n\nFocus on the app\n\nhttp://heroku.com/"
 ];
 
 var T = new Twit({
