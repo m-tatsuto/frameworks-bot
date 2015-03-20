28b58858b240524cbe29588a449b9702dff9d28a
t.maetsu <t.maetsu@lexues.co.jp>
2015-03-20 15:18:05 +0900

    add new message

--- bin/index.js
+++ bin/index.js
@@ -33,7 +33,8 @@ var frameworkTweets = [
  "sensu #Monitoring #Framework\n\nThe open source monitoring framework.\n\nhttp://sensuapp.org/",
  "KILL BILL #Billing #Payment #Platform\n\nOpen Source Subscription Billing & Payment Platform.\n\nhttp://killbill.io/",
  "testmunk #Service #Test\n\nTHE FASTEST WAY TO TEST YOUR APP\n\nhttp://testmunk.com/",
- "UNDERSCORES #Wordpress #Theme\n\nCREATE YOUR UNDERSCORES BASED THEME\n\nhttp://underscores.me/"
+ "UNDERSCORES #Wordpress #Theme\n\nCREATE YOUR UNDERSCORES BASED THEME\n\nhttp://underscores.me/",
+ "Yeoman #Tool #npm #Web\n\nTHE WEB'S SCAFFOLDING TOOL FOR MODERN WEBAPPS\n\nhttp://yeoman.io/"
 ];
 
 var T = new Twit({
